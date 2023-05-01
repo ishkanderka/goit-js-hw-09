b674cf12f3e2caeec74734a8e682497c0a1e2c69
Victoria Shmidt <reachme@victoriashmidt.com>
2023-05-01 15:24:55 -0600

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,8 @@
+const startBtn = document.querySelector('button[data-start="start"]');
+const stopBtn = document.querySelector('button[data-stop="stop"]');
 
+function onStartBtnClick() {
+  console.log('click', onStartBtnClick);
+}
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
