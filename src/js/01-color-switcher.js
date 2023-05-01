86c6e5b5ea076f6e94074a40373fb67902aae952
Victoria Shmidt <reachme@victoriashmidt.com>
2023-05-01 15:52:01 -0600

    Task 1 done 👩🏻‍💻

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,8 +1,25 @@
-const startBtn = document.querySelector('button[data-start="start"]');
-const stopBtn = document.querySelector('button[data-stop="stop"]');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
+
+startBtn.disabled = false;
 
 function onStartBtnClick() {
-  console.log('click', onStartBtnClick);
+  timerId = setInterval(() => {
+    return (body.style.backgroundColor = `#${Math.floor(
+      Math.random() * 16777215
+    )
+      .toString(16)
+      .padStart(6, 0)}`);
+  }, 1000);
+  startBtn.disabled = true;
 }
+function onStopBtnClick() {
+  clearInterval(timerId);
+  body.style.backgroundColor = 'transparent';
+  startBtn.disabled = false;
+}
+
 startBtn.addEventListener('click', onStartBtnClick);
 stopBtn.addEventListener('click', onStopBtnClick);
